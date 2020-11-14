02efbd932610e399611a5357368fb0967d69c659
Ice <richard.jun.noh@gmail.com>
2020-11-14 01:13:19 -0600

    Add raw_playlistitemsjsonlist.txt
    Special print method from https://stackoverflow.com/questions/14630288/unicodeencodeerror-charmap-codec-cant-encode-character-maps-to-undefined

--- main.py
+++ main.py
@@ -1,15 +1,25 @@
+# -*- coding: utf-8 -*-
 #Unus Annus Playlist ID:
 #   UUIcgBZ9hEJxHv6r_jDYOMqg
 
 #modified youtube api sample code
 
 import os
-
+import sys
 import google_auth_oauthlib.flow
 import googleapiclient.discovery
 import googleapiclient.errors
 
 scopes = ["https://www.googleapis.com/auth/youtube.readonly"]
+
+
+def uprint(*objects, sep=' ', end='\n', file=sys.stdout):
+    enc = file.encoding
+    if enc == 'UTF-8':
+        print(*objects, sep=sep, end=end, file=file)
+    else:
+        f = lambda obj: str(obj).encode(enc, errors='backslashreplace').decode(enc)
+        print(*map(f, objects), sep=sep, end=end, file=file)
 
 def main():
     # Disable OAuthlib's HTTPS verification when running locally.
@@ -28,7 +38,6 @@ def main():
         api_service_name, api_version, credentials=credentials)
 
     # Get all playlist items
-    total_expected_videos = 365
     list_of_playlistItems = []
 
     request = youtube.playlistItems().list(
@@ -53,7 +62,12 @@ def main():
             )
         else:
             has_next = False
-    
+    uprint(list_of_playlistItems)
+
+    #Get list of video IDs
+    #for playlistItems in list_of_playlistItems:
+#for item in playlistItems["items"]:
+
 
 
 if __name__ == "__main__":
